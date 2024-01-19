a89818c21a499bbf3c56eb184182f75afd1f727e
DhruvDaherawala <123897835+DhruvDaherawala@users.noreply.github.com>
2024-01-20 01:20:15 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
-echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+
+# Activate virtual environment
+source /vercel/path0/venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run Django migrations or other setup commands
+python manage.py migrate
+
+# Continue with the build process
+# ...
